096f8eedadd665c5d166579fddc9a00c4c838009
Matthew Zhou <emzeegotanemail2@gmail.com>
2023-12-27 13:43:17 -0500

    Update legal password chars

--- src/password_utils.py
+++ src/password_utils.py
@@ -1,11 +1,16 @@
 import secrets
 
 
+legal_password_characters = "~`!@#$%^&*()_-+={[}]|:;'<,>.?/abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+
 def generate_password(length: int = 16) -> str:
     """
     Generate a password with the given length (default 16)
     """
-    password = secrets.token_urlsafe(length)[:length]
+    password = "".join(
+        [secrets.choice(legal_password_characters) for _ in range(length)]
+    )
 
     while password.isalnum():
         password = secrets.token_urlsafe(length)[:length]
